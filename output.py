b79d5266d2e844967a9a8efbb182d32a4aabe588
Łukasz Hryniuk <lukasz.hryniuk@wp.pl>
2018-03-15 22:54:40 +0100

    Add pgm generating function

--- output.py
+++ output.py
@@ -13,6 +13,19 @@ def to_pbm(luminescence_matrix):
 
     return "\n".join(lines) + "\n"
 
+def to_pgm(luminescence_matrix, max_value=255):
+    lines = []
+    lines.append("P2")
+    lines.append("{} {}".format(len(luminescence_matrix[0]),
+                                len(luminescence_matrix)))
+    lines.append("{}".format(max_value))
+
+    scale = lambda x: min(max_value, max(0, int(x * max_value)))
+    for row in luminescence_matrix:
+        lines.append(" ".join(((str(scale(k))) for k in row)))
+
+    return "\n".join(lines) + "\n"
+
 
 if __name__ == '__main__':
     data = to_pbm(ray_tracer.luminescence_from_objects(
